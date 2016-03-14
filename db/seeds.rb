5b640e5c7a1e1f291f726de724cadd6980e00107
ypan887 <ypan887@gmail.com>
2016-03-14 00:22:10 -0700

    Add seeds to database. Add callbacks to skip verify_authenticity_token on update and create action. Update Readme.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+customers = Customer.create([{ name: "Robert C Motyka", address: "1012 Maryland Avenue, Pinellas city", zip_code: "34624", country: "USA" },{ name: "Willie L Gage", address: "3589 Terra Cotta Street, Federal Dam city", zip_code: "56641", country: "USA" }])
+
+categories = Category.create([{ name: "Vitamins & Supplements", description:"General vitamins and health supplements" },{ name:"Small appliance ", description:"Portable or semi-portable machines for kitchen and home" }])
+
+merchants = Merchant.create([{ name: "Amazon"},{ name: "Bloomingdales" }])
+
+products = Product.create([{ name: "Enfamil Liquid Vitamin D", category_id: categories.first.id , price: 10, cost: 6.85 },{ name: "Baby Ddrops liquid Vitamin D", category_id: categories.first.id, price: 16, cost: 12},{ name: "Irobot 880", category_id: categories.last.id , price: 800, cost:600 }])
+
+Order.create([{ customer_id: customers.first.id, order_date: "2016-03-06", :order_details_attributes => [{ product_id: products[0].id, quantity: 4 },{ product_id: products[1].id, quantity: 4 },{ product_id: products[2].id, quantity: 1 }]}, { customer_id: customers.last.id, order_date: "2016-04-01", :order_details_attributes => [{ product_id: products[2].id, quantity: 2 }]}])
+
+Shipment.create([{ tracking_number: "6KUV67845987KI", ship_cost: 40, ship_date: "2016-03-10", :shipment_details_attributes => [{ product_id: products[0].id, quantity: 4 },{ product_id: products[1].id, quantity: 4 },{ product_id: products[2].id, quantity: 1 }]}, { tracking_number: "4HGT34596837TF", ship_cost: 100, ship_date: "2016-04-06", :shipment_details_attributes => [{ product_id: products[2].id, quantity: 1 }]}])
+
+Supply.create([{ order_date: "2016-03-07", merchant_id: merchants.first.id, :supply_details_attributes => [{ product_id: products[0].id, quantity: 4 },{ product_id: products[1].id, quantity: 4 }]},{ order_date: "2016-03-07", merchant_id: merchants.first.id, :supply_details_attributes => [{ product_id: products[2].id, quantity: 1 }]}, { order_date: "2016-04-02", merchant_id: merchants.last.id, :supply_details_attributes => [{ product_id: products[2].id, quantity: 2 }]}])
--- readme.md
+++ readme.md
@@ -9,5 +9,49 @@ I am building this app becasue my girl need a easy to use solution for her small
 
 ## Installation
 
+```
+bundle install
+rake db:migrate
+rake db:setup  # Only if you want to play around with seeded data to see how everything is working in this api
+rails s
+```
 
-## Example
+## Features
+This api serves as a tool to keep an record on the products inventory. That includes all the information on custermers and their orders, products with categories, shipments of products, and supplies of product inventory.
+
+## Examples
+
+If you choose to seed the database, you can start explore the api
+
+### checkout categories and the products
+```
+curl localhost:3000/categoris
+curl localhost:3000/category/1/products
+```
+
+### checkout customers, and their orders
+
+```
+curl localhost:3000/customers
+curl localhost:3000/customer/1/orders
+```
+
+### checkout supplier merchants, and their supply records
+```
+curl localhost:3000/merchants
+curl localhost:3000/merchant/1/supplies
+```
+
+### checkout monthly statement. 
+Statement will be updated whenever any relative sales information has changed on that paticular month. For example, if a orders record on Apr 12 2016 has changed, the income statement for that month will be recalculated and updated. If the price or cost of a product has changed, every monthly statements that happens after the created date of the product will be updated as well.
+
+```
+curl localhost:3000/statements
+```
+
+you can also modify your sales record like this:
+```
+curl -v -H "Content-Type: application/json" -X PATCH http://localhost:3000/shipments/1 -d '{ "ship_cost": "20" }'
+
+curl -v -H "Content-Type: application/json" -X PATCH http://localhost:3000/orders/1 -d '{ "order_date": "2016-04-05", "order_details_attributes": [{ "id": "6", "product_id": "1", "quantity": "4"}]}'
+```
