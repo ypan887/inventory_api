c644ebb830fbe5d9e213ba4553820032f7a875e5
ypan887 <ypan887@gmail.com>
2016-04-02 00:02:34 -0700

    delete migration to modify_references_on_supply_details

--- db/migrate/20160308234223_modify_references_on_supply_details.rb
+++
@@ -1,6 +0,0 @@
-class ModifyReferencesOnSupplyDetails < ActiveRecord::Migration
-  def change
-    rename_column :supply_details, :products_id, :product_id
-    rename_column :supply_details, :supplies_id, :supply_id
-  end
-end
