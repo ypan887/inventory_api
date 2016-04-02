c6c1b060be051e9a93af501b754e51e696e56c96
ypan887 <ypan887@gmail.com>
2016-04-01 18:47:27 -0700

    migration bugfix for supply-details table

--- db/migrate/20160307093343_create_supply_details.rb
+++ db/migrate/20160307093343_create_supply_details.rb
@@ -2,8 +2,8 @@ class CreateSupplyDetails < ActiveRecord::Migration
   def change
     create_table :supply_details do |t|
       t.integer :quantity
-      t.references :supplies, index: true, foreign_key: true
-      t.references :products, index: true, foreign_key: true
+      t.references :supply, index: true, foreign_key: true
+      t.references :product, index: true, foreign_key: true
       t.boolean :deliver_check, default: false
 
       t.timestamps null: false
